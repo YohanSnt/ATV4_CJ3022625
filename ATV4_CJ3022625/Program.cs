2097a99528b605a3b08a344122eb00d66a595727
YohanSnt <yohan.s@aluno.ifsp.edu.br>
2024-03-20 10:03:35 -0300

    Ex1 feito

--- ATV4_CJ3022625/Program.cs
+++ ATV4_CJ3022625/Program.cs
@@ -6,56 +6,117 @@ namespace ATV4_CJ3022625
     {
         static void Main(string[] args)
         {
-            int c = 5;
-            float n, n1, a, s, m, d;
-
-            //Entrada números
-            Console.WriteLine("<<CALCULADORA>>\n");
-            Console.WriteLine("Digite um número: ");
-            n=float.Parse(Console.ReadLine());
+            int ex;
+            Console.WriteLine("1- Exercício 1");
+            Console.WriteLine("2- Exercício 2");
+            Console.WriteLine("3- Exercício 3");
+            Console.WriteLine("4- Exercício 4");
+            ex = int.Parse(Console.ReadLine());
            
-            Console.WriteLine("Digite outro número: ");
-            n1= float.Parse(Console.ReadLine());
-
-            //Operações
-            Console.WriteLine("\n<<Operações>>\n");
-            Console.WriteLine("1- Adição\n");
-            Console.WriteLine("2- Subtração\n");
-            Console.WriteLine("3- Multiplicação\n");
-            Console.WriteLine("4-Divisão\n");
-            Console.WriteLine("Opção: ");
-            c=int.Parse(Console.ReadLine());
-
-           //OPÇÕES
-            switch (c)
+            switch (ex)
             {
+               
+                //EX1
                 case 1:
-                    Console.WriteLine("Adição");
-                    a = n + n1;
-                    Console.WriteLine("Resultado = {0}",a);
-                    break;
+                    int dia;
+                    Console.WriteLine("Dias da semana:\n");
+                    Console.WriteLine("1\n");
+                    Console.WriteLine("2\n");
+                    Console.WriteLine("3\n");
+                    Console.WriteLine("4\n");
+                    Console.WriteLine("5\n");
+                    Console.WriteLine("6\n");
+                    Console.WriteLine("7\n");
+                    dia=int.Parse(Console.ReadLine());
 
-                case 2:
-                    Console.WriteLine("Subtração");
-                    s = n - n1;
-                    Console.WriteLine("Resultado = {0}", s);
-                    break;
+                    switch (dia)
+                    {
+                        case 1:
+                            Console.WriteLine("Domingo");
+                            break;
 
-                case 3:
-                    Console.WriteLine("Multiplicação");
-                    m = n * n1;
-                    Console.WriteLine("Resultado = {0}", m);
-                    break;
+                        case 2:
+                            Console.WriteLine("Segunda");
+                            break;
+                        case 3:
+                            Console.WriteLine("Terça");
+                            break;
 
-                case 4:
-                    Console.WriteLine("Divisão");
-                    d = n / n1;
-                    Console.WriteLine("Resultado = {0}", d);
-                    break;
+                        case 4:
+                            Console.WriteLine("Quarta");
+                            break;
+                        case 5:
+                            Console.WriteLine("Quinta");
+                            break;
 
-               default:
-                    Console.WriteLine("Opção inválida");
-                    break;
+                        case 6:
+                            Console.WriteLine("Sexta");
+                            break;
+
+                        case 7:
+                            Console.WriteLine("Sábado");
+                            break;
+
+                    }
+
+                break;
+                    
+
+
+
+
+
+            int c = 5;
+                    float n, n1, a, s, m, d;
+
+                    //Entrada números
+                    Console.WriteLine("<<CALCULADORA>>\n");
+                    Console.WriteLine("Digite um número: ");
+                    n = float.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite outro número: ");
+                    n1 = float.Parse(Console.ReadLine());
+
+                    //Operações
+                    Console.WriteLine("\n<<Operações>>\n");
+                    Console.WriteLine("1- Adição\n");
+                    Console.WriteLine("2- Subtração\n");
+                    Console.WriteLine("3- Multiplicação\n");
+                    Console.WriteLine("4-Divisão\n");
+                    Console.WriteLine("Opção: ");
+                    c = int.Parse(Console.ReadLine());
+
+                    //OPÇÕES
+                    switch (c)
+                    {
+                        case 1:
+                            Console.WriteLine("Adição");
+                            a = n + n1;
+                            Console.WriteLine("Resultado = {0}", a);
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Subtração");
+                            s = n - n1;
+                            Console.WriteLine("Resultado = {0}", s);
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Multiplicação");
+                            m = n * n1;
+                            Console.WriteLine("Resultado = {0}", m);
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Divisão");
+                            d = n / n1;
+                            Console.WriteLine("Resultado = {0}", d);
+                            break;
+
+                        default:
+                            Console.WriteLine("Opção inválida");
+                            break;
+                    }
             }
         }
     }
